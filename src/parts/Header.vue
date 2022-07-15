357c077a4d6d9dc23c6c736b5d9edc9f13b41c3e
Valerie Kraucunas <valeriekraucunas@gmail.com>
2022-07-14 20:50:19 -0600

    style updates

--- src/parts/Header.vue
+++ src/parts/Header.vue
@@ -34,7 +34,7 @@ header {
   padding: 6rem;
   height: 30vh;
   min-height: 80vh;
-  background-color: rgb(13, 58, 45, 0.3);
+  background-color: rgba(13, 58, 45, 0.4);
 
   .header-contents {
     width: 90%;
